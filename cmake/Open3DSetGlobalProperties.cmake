80c2b4e78b107819c61cb0ccf45c040e573215d1
Yixing Lao <yixing.lao@gmail.com>
2021-10-19 05:41:23 -0700

    ABI must be 1

--- cmake/Open3DSetGlobalProperties.cmake
+++ cmake/Open3DSetGlobalProperties.cmake
@@ -10,14 +10,8 @@ function(open3d_set_global_properties target)
         $<INSTALL_INTERFACE:${Open3D_INSTALL_INCLUDE_DIR}>
     )
 
-    # Required for static linking zeromq
-    target_compile_definitions(${target} PRIVATE ZMQ_STATIC)
-
-    if (GLIBCXX_USE_CXX11_ABI)
-        target_compile_definitions(${target} PUBLIC _GLIBCXX_USE_CXX11_ABI=1)
-    else()
-        target_compile_definitions(${target} PUBLIC _GLIBCXX_USE_CXX11_ABI=0)
-    endif()
+    # target_compile_definitions(${target} PUBLIC _GLIBCXX_USE_CXX11_ABI=1)
+    target_compile_definitions(${target} PUBLIC _GLIBCXX_USE_CXX11_ABI=0)
 
 
 endfunction()
